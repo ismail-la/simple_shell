1bfdf62f1f49d4d5e358e2259341d39a062a7a0a
ismail-la <lahbariismail@gmail.com>
2023-05-24 21:30:07 -0700

    update

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * main - the entry point
+ * main - entry point
  * @argc: the Argument count
  * @argv: the Argument vector
  * Return: Returns status 0 on success
@@ -14,39 +14,37 @@ if (argc > 1)
 if (argv[1] != NULL)
 	readFD(argv[1], argv);
 signal(SIGINT, Handle_signal);
-while (status)
-{
-j++;
-if (isatty(STDIN_FILENO))
-	_prompt();
-LineInput = readgetline();
-if (LineInput[0] == '\0')
-	continue;
-F_history(LineInput);
-Sep_commands = Function_separator(LineInput);
-for (i = 0; Sep_commands[i] != NULL; i++)
-{
-cmd = parse_command(Sep_commands[i]);
-if (str_cmp(cmd[0], "exit") == 0)
-{
-	free(Sep_commands);
-	Status_exit__bul(cmd, LineInput, argv, j, Case_stat);
-}
-else if (cmd_check_builtin(cmd) == 0)
-{
-	Case_stat = handle_builtin(cmd, Case_stat);
-	free(cmd);
-	continue;
-}
-else
-{
-	Case_stat = fork_check_cmd(cmd, LineInput, j, argv);
-}
-free(cmd);
-}
-free(LineInput);
-free(Sep_commands);
-wait(&Case_stat);
-}
-return (Case_stat);
+	while (status)
+	{
+		j++;
+		if (isatty(STDIN_FILENO))
+			_prompt();
+		LineInput = readgetline();
+		if (LineInput[0] == '\0')
+			continue;
+		F_history(LineInput);
+		Sep_commands = Function_separator(LineInput);
+		for (i = 0; Sep_commands[i] != NULL; i++)
+		{
+			cmd = parse_command(Sep_commands[i]);
+			if (str_cmp(cmd[0], "exit") == 0)
+			{
+				free(Sep_commands);
+				Status_exit__bul(cmd, LineInput, argv, j, Case_stat);
+			}
+			else if (cmd_check_builtin(cmd) == 0)
+			{
+				Case_stat = handle_builtin(cmd, Case_stat);
+				free(cmd);
+				continue;
+			}
+			else
+				Case_stat = fork_check_cmd(cmd, LineInput, j, argv);
+			free(cmd);
+		}
+		free(LineInput);
+		free(Sep_commands);
+		wait(&Case_stat);
+	}
+	return (Case_stat);
 }
